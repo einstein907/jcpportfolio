ac742dc3db3abc3f442b0a0bfc014dbad790296f
Jenna Palmer <jenna.palmer@slalom.com>
2018-10-11 22:22:49 -0500

    added pinterest masonry layout to front page and am figuring out main design

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,9 +13,7 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   /*
@@ -26,19 +24,17 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
-
+  plugins: [{ src: '~/plugins/vue-masonry', ssr: false }],
   /*
   ** Nuxt.js modules
   */
-  modules: [,
+  modules: [
+    ,
     // Doc: https://bootstrap-vue.js.org/docs/
     'bootstrap-vue/nuxt'
   ],
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,46 @@
 <template>
-  <div>
-    <header>
-      <NavigationBar />
-    </header>
-    <body>
-      <div class="container">
-        <div class="row">
-          <div class="col-3">
-            <LeftNavBar />
-          </div>
-          <div class="col-9">
-            <div class="row">
-              <nuxt-link :to="`/pieces/${piece.id}`" class="col-sm" v-for="(piece, index) in pieces" :key="index">
-                <div class="card" style="width: 18rem;">
-                  <img class="card-img-top" :src="`${piece.mainImage}`" :alt="`${piece.title}`">
-                  <div class="card-body">
-                    <p class="card-text">
-                      "{{ piece.title }}" - {{ piece.price }}<br>
-                      {{ piece.size }}; {{ piece.medium }}
-                    </p>
-                  </div>
-                </div>
-              </nuxt-link>
-            </div>
-          </div>
+  <div class="container">
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="#" class="title">PORTRAITS BY PALMER</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+          <b-nav-item href="#" class="menu-item">portfolio</b-nav-item>
+          <b-nav-item href="#" class="menu-item">prints + commissions</b-nav-item>
+          <b-nav-item href="#" class="menu-item">contact</b-nav-item>
+          <b-nav-item href="#" class="menu-item">news</b-nav-item>
+
+        </b-navbar-nav>  
+
+      </b-collapse>
+    </b-navbar>
+
+    <no-ssr>
+      <div v-masonry 
+        transition-duration="3s" 
+        item-selector=".item" 
+        class="masonry-container grid" 
+        fit-width="true"
+      >
+        <div v-masonry-tile class="item" :key="index" v-for="(piece, index) in pieces">
+          <img class="art-card" :src="`${piece.mainImage}`"></img>
         </div>
       </div>
-    </body>
+    </no-ssr>
+
+    <Footer />
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import pieces from '../static/pieces.json';
-import LeftNavBar from "~/components/leftnavbar.vue"
-import NavigationBar from "~/components/navigationbar.vue";
+import NoSSR from 'vue-no-ssr';
+import Footer from '~/components/footer.vue';
 
 export default {
   fetch({ store }) {
@@ -43,9 +49,18 @@ export default {
   computed: {
     ...mapState(['pieces'])
   },
-  components: { LeftNavBar, NavigationBar }
+  components: {
+    'no-ssr': NoSSR,
+    Footer
+  },
+  mounted () {
+    if (typeof this.$redrawVueMasonry === 'function') {
+      this.$redrawVueMasonry()
+    }
+  }
 }
 </script>
 
 <style>
+@import '~/assets/styles.scss';
 </style>
